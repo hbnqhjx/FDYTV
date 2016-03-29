bdd11a79ad29a4c9a3ca11052450fe8079eb7d4f
hbnqhjx <981815998@qq.com>
2016-03-29 17:23:00 +0800

    1

---
+++ DYTV/DYTV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DYTV
+//
+//  Created by 韩嘉鑫 on 16/3/29.
+//  Copyright © 2016年 hjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
